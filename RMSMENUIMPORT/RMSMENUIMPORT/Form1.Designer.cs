a9505e7f0ff42fff595e8a1be6eee8a13ecfe16b
Pritesh Bhavsar <bhavsarpritesh.mca@gmail.com>
2022-01-04 14:16:06 +0530

    first commit

---
+++ RMSMENUIMPORT/RMSMENUIMPORT/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace RMSMENUIMPORT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pnlmain = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnbrowse = new System.Windows.Forms.Button();
+            this.txtexcelfilepath = new System.Windows.Forms.TextBox();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.btnshow = new System.Windows.Forms.Button();
+            this.btnimport = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnldata = new System.Windows.Forms.Panel();
+            this.dtgdata = new System.Windows.Forms.DataGridView();
+            this.pnlmain.SuspendLayout();
+            this.pnldata.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgdata)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlmain
+            // 
+            this.pnlmain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlmain.Controls.Add(this.label2);
+            this.pnlmain.Controls.Add(this.btnbrowse);
+            this.pnlmain.Controls.Add(this.txtexcelfilepath);
+            this.pnlmain.Controls.Add(this.btnclear);
+            this.pnlmain.Controls.Add(this.btnshow);
+            this.pnlmain.Controls.Add(this.btnimport);
+            this.pnlmain.Controls.Add(this.button2);
+            this.pnlmain.Controls.Add(this.button4);
+            this.pnlmain.Controls.Add(this.button1);
+            this.pnlmain.Controls.Add(this.label1);
+            this.pnlmain.Location = new System.Drawing.Point(1, 3);
+            this.pnlmain.Name = "pnlmain";
+            this.pnlmain.Size = new System.Drawing.Size(496, 139);
+            this.pnlmain.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Select Menu (.xls File)";
+            // 
+            // btnbrowse
+            // 
+            this.btnbrowse.ForeColor = System.Drawing.Color.Blue;
+            this.btnbrowse.Location = new System.Drawing.Point(418, 70);
+            this.btnbrowse.Name = "btnbrowse";
+            this.btnbrowse.Size = new System.Drawing.Size(65, 26);
+            this.btnbrowse.TabIndex = 3;
+            this.btnbrowse.Text = ". . .";
+            this.btnbrowse.UseVisualStyleBackColor = true;
+            this.btnbrowse.Click += new System.EventHandler(this.btnbrowse_Click);
+            // 
+            // txtexcelfilepath
+            // 
+            this.txtexcelfilepath.Location = new System.Drawing.Point(8, 71);
+            this.txtexcelfilepath.Name = "txtexcelfilepath";
+            this.txtexcelfilepath.ReadOnly = true;
+            this.txtexcelfilepath.Size = new System.Drawing.Size(405, 25);
+            this.txtexcelfilepath.TabIndex = 2;
+            // 
+            // btnclear
+            // 
+            this.btnclear.ForeColor = System.Drawing.Color.Red;
+            this.btnclear.Location = new System.Drawing.Point(91, 99);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(80, 31);
+            this.btnclear.TabIndex = 1;
+            this.btnclear.Text = "Clear";
+            this.btnclear.UseVisualStyleBackColor = true;
+            this.btnclear.Click += new System.EventHandler(this.btnclear_Click);
+            // 
+            // btnshow
+            // 
+            this.btnshow.ForeColor = System.Drawing.Color.Blue;
+            this.btnshow.Location = new System.Drawing.Point(8, 98);
+            this.btnshow.Name = "btnshow";
+            this.btnshow.Size = new System.Drawing.Size(80, 31);
+            this.btnshow.TabIndex = 1;
+            this.btnshow.Text = "Show";
+            this.btnshow.UseVisualStyleBackColor = true;
+            this.btnshow.Click += new System.EventHandler(this.btnshow_Click);
+            // 
+            // btnimport
+            // 
+            this.btnimport.ForeColor = System.Drawing.Color.Green;
+            this.btnimport.Location = new System.Drawing.Point(333, 100);
+            this.btnimport.Name = "btnimport";
+            this.btnimport.Size = new System.Drawing.Size(80, 31);
+            this.btnimport.TabIndex = 1;
+            this.btnimport.Text = "Import";
+            this.btnimport.UseVisualStyleBackColor = true;
+            this.btnimport.Click += new System.EventHandler(this.btnimport_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(385, 33);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 31);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Hide All Item";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(270, 1);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(115, 31);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Delete All Group";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(385, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Delete All Item";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(4, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IMPORT MENU";
+            // 
+            // pnldata
+            // 
+            this.pnldata.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnldata.Controls.Add(this.dtgdata);
+            this.pnldata.Location = new System.Drawing.Point(1, 146);
+            this.pnldata.Name = "pnldata";
+            this.pnldata.Size = new System.Drawing.Size(496, 344);
+            this.pnldata.TabIndex = 1;
+            // 
+            // dtgdata
+            // 
+            this.dtgdata.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgdata.ColumnHeadersVisible = false;
+            this.dtgdata.Location = new System.Drawing.Point(3, 3);
+            this.dtgdata.MultiSelect = false;
+            this.dtgdata.Name = "dtgdata";
+            this.dtgdata.RowHeadersVisible = false;
+            this.dtgdata.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dtgdata.Size = new System.Drawing.Size(488, 336);
+            this.dtgdata.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(197)))), ((int)(((byte)(224)))), ((int)(((byte)(245)))));
+            this.ClientSize = new System.Drawing.Size(500, 497);
+            this.Controls.Add(this.pnldata);
+            this.Controls.Add(this.pnlmain);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IMPORT MENU";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlmain.ResumeLayout(false);
+            this.pnlmain.PerformLayout();
+            this.pnldata.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgdata)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlmain;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnbrowse;
+        private System.Windows.Forms.TextBox txtexcelfilepath;
+        private System.Windows.Forms.Button btnimport;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnshow;
+        private System.Windows.Forms.Panel pnldata;
+        private System.Windows.Forms.DataGridView dtgdata;
+        private System.Windows.Forms.Button btnclear;
+    }
+}
+
---
+++ RMSMENUIMPORT/RMSMENUIMPORT/Form1.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RMSMENUIMPORT
+{
+    public partial class Form1 : Form
+    {
+        private clsmssqldbfunction mssql = new clsmssqldbfunction();
+
+        public static string sheetnm = "Sheet1";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnbrowse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();  //create openfileDialog Object
+                openFileDialog1.Filter = "XML Files (*.xml; *.xls; *.xlsx; *.xlsm; *.xlsb) |*.xml; *.xls; *.xlsx; *.xlsm; *.xlsb";//open file format define Excel Files(.xls)|*.xls| Excel Files(.xlsx)|*.xlsx| 
+                openFileDialog1.FilterIndex = 3;
+                openFileDialog1.Multiselect = false;
+                openFileDialog1.Title = "Open Text File-R13";   //define the name of openfileDialog
+                openFileDialog1.InitialDirectory = Path.Combine(Application.StartupPath, @"DEMO");
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    string pathName = openFileDialog1.FileName;
+                    this.txtexcelfilepath.Text = pathName;
+
+                    string fileName = System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.FileName);
+                    DataTable tbContainer = new DataTable();
+                    string strConn = string.Empty;
+                    string sheetName = fileName;
+
+                    FileInfo file = new FileInfo(pathName);
+                    if (!file.Exists) { throw new Exception("Error, file doesn't exists!"); }
+                    string extension = file.Extension;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString() + " Error occures in btnbakupbrows_Click())", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnshow_Click(object sender, EventArgs e)
+        {
+
+            string pathName = "";
+            string fileName = "";
+            DataSet ds1 = new DataSet();
+
+            try
+            {
+
+                pathName = this.txtexcelfilepath.Text.Trim();
+                fileName = System.IO.Path.GetFileNameWithoutExtension(pathName);
+
+                if (fileName.Trim() == "")
+                {
+                    throw new Exception("Please Select Menu file.");
+                }
+
+                FileInfo file = new FileInfo(pathName);
+                if (!file.Exists) { throw new Exception("Error, file doesn't exists!"); }
+
+                ds1 = this.Import(pathName, false);
+                this.dtgdata.DataSource = ds1.Tables[0];
+                this.dtgdata.Refresh();
+
+                if (ds1.Tables[0].Rows.Count > 0)
+                {
+                    this.btnimport.Visible = true;
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString() + " Error occures in btnbakupbrows_Click())", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            finally
+            {
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.txtexcelfilepath.Text = "";
+            this.btnimport.Visible = false;
+            this.ClearDatagrid();
+        }
+
+
+        public DataSet Import(string filename, bool headers = true)
+        {
+            var _xl = new Microsoft.Office.Interop.Excel.Application();
+            var wb = _xl.Workbooks.Open(filename);
+            var sheets = wb.Sheets;
+            DataSet dataSet = null;
+
+            try
+            {
+                if (sheets != null && sheets.Count != 0)
+                {
+                    dataSet = new DataSet();
+                    foreach (var item in sheets)
+                    {
+                        var sheet = (Microsoft.Office.Interop.Excel.Worksheet)item;
+                        DataTable dt = null;
+                        if (sheet != null)
+                        {
+                            dt = new DataTable();
+                            var ColumnCount = ((Microsoft.Office.Interop.Excel.Range)sheet.UsedRange.Rows[1, Type.Missing]).Columns.Count;
+                            var rowCount = ((Microsoft.Office.Interop.Excel.Range)sheet.UsedRange.Columns[1, Type.Missing]).Rows.Count;
+
+                            for (int j = 0; j < ColumnCount; j++)
+                            {
+                                var cell = (Microsoft.Office.Interop.Excel.Range)sheet.Cells[1, j + 1];
+                                var column = new DataColumn(headers ? cell.Value : string.Empty);
+                                dt.Columns.Add(column);
+                            }
+
+                            for (int i = 0; i < rowCount; i++)
+                            {
+                                var r = dt.NewRow();
+                                for (int j = 0; j < ColumnCount; j++)
+                                {
+                                    var cell = (Microsoft.Office.Interop.Excel.Range)sheet.Cells[i + 1 + (headers ? 1 : 0), j + 1];
+                                    r[j] = cell.Value;
+                                }
+                                dt.Rows.Add(r);
+                            }
+
+                        }
+                        dataSet.Tables.Add(dt);
+                    }
+                }
+                _xl.Quit();
+                return dataSet;
+            }
+            catch (Exception)
+            {
+                return dataSet;
+            }
+        }
+
+        private void btnimport_Click(object sender, EventArgs e)
+        {
+            string sqlinsert = "";            
+            StringBuilder duplicateiteminfo = new StringBuilder("");
+            try
+            {
+                if (!mssql.OpenMsSqlConnection())
+                {
+                    MessageBox.Show("Please Check Your Connection");
+                }
+
+                DataTable dt =   mssql.DataGridView2DataTable(this.dtgdata, "");
+                DataTable dticode = new DataTable();
+
+                if (dt.Rows.Count > 0)
+                {
+                    foreach (DataRow item in dt.Rows)
+                    {
+                        mssql.OpenMsSqlConnection();
+
+                        string icode1= item["column1"] + "".Trim();
+                        string strchq = "SELECT ICODE FROM MSTITEM WHERE ISNULL(DELFLG,0)=0 AND ICODE = '" + icode1 + "'";
+
+                        dticode = mssql.FillDataTable(strchq, "MSTITEM");
+
+                        if (dticode.Rows.Count <= 0)
+                        {
+                            sqlinsert = "INSERT INTO MSTITEM (ICODE,INAME,IRATE,IGRPRID,DELFLG,ADATETIME,IPNAME,ISHIDEITEM) values (@ICODE,@INAME,@IRATE,@IGRPRID,@DELFLG,@ADATETIME,@IPNAME,@ISHIDEITEM)";
+
+                            if (item["column2"] + "".Trim() != "")                            {
+
+                                SqlCommand cmd = new SqlCommand(sqlinsert, clsmssqldbfunction.mssqlcon);
+
+                                cmd.Parameters.Add("@ICODE", SqlDbType.VarChar);
+                                cmd.Parameters["@ICODE"].Value = item["column1"] + "".Trim();
+
+                                cmd.Parameters.Add("@INAME", SqlDbType.VarChar);
+                                cmd.Parameters["@INAME"].Value = item["column2"] + "".Trim();
+
+                                cmd.Parameters.Add("@IRATE", SqlDbType.Decimal);
+                                decimal rate1;
+                                decimal.TryParse(item["column3"] + "", out rate1);
+                                cmd.Parameters["@IRATE"].Value = rate1;
+
+                                cmd.Parameters.Add("@IGRPRID", SqlDbType.Int);
+                                int igrprid1;
+                                int.TryParse(item["column4"] + "", out igrprid1);
+                                cmd.Parameters["@IGRPRID"].Value = igrprid1;                             
+                                
+                                cmd.Parameters.Add("@DELFLG", SqlDbType.Bit);
+                                cmd.Parameters["@DELFLG"].Value = 0;
+
+                                cmd.Parameters.Add("@ADATETIME", SqlDbType.DateTime);
+                                cmd.Parameters["@ADATETIME"].Value = DateTime.Now;
+
+                                cmd.Parameters.Add("@IPNAME", SqlDbType.VarChar);
+                                cmd.Parameters["@IPNAME"].Value = item["column2"] + "".Trim();
+
+                                cmd.Parameters.Add("@ISHIDEITEM", SqlDbType.Int);
+                                cmd.Parameters["@ISHIDEITEM"].Value = 0;
+
+                                cmd.ExecuteNonQuery();
+                            }
+                        }
+                        else
+                        {
+                            duplicateiteminfo.Append(icode1 + ",");
+                        }
+                    }
+
+                    MessageBox.Show("IMPORT SUCESSFULLY. " + System.Environment.NewLine + "WITH FOLLOWING INFORMATION." + System.Environment.NewLine + " * DUPLICATE CODE FOUND : " + duplicateiteminfo +" WHICH IS NOT IMPORTED.");
+
+                    this.Form1_Load(sender, e);
+                }
+
+            }
+            catch (Exception ex)
+            { }
+            finally
+            {
+                mssql.CloseMsSqlConnection();
+            }
+        }
+
+        private bool ClearDatagrid()
+        {
+            try
+            {
+                this.dtgdata.DataSource = null;
+                this.dtgdata.Refresh();
+
+                return true;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+        }
+
+        private bool Checkconnection()
+        {
+            try
+            {
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to Delete the All Item ?.", "", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+            if (dialogResult == DialogResult.Yes)
+            {
+                mssql.OpenMsSqlConnection();
+                string str1 = "DELETE FROM MSTITEM";
+                mssql.ExecuteMsSqlCommand_NoMsg(str1);
+                MessageBox.Show("Delete Sucessfully");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to Delete the All Group ?.", "", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+            if (dialogResult == DialogResult.Yes)
+            {
+                mssql.OpenMsSqlConnection();
+                string str1 = "DELETE FROM MSTITEMGROUP";
+                mssql.ExecuteMsSqlCommand_NoMsg(str1);
+                MessageBox.Show("Delete Sucessfully");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to Hide All Item ?.", "", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+            if (dialogResult == DialogResult.Yes)
+            {
+                mssql.OpenMsSqlConnection();
+                string str1 = "update mstitem set ISHIDEITEM = 1";
+                mssql.ExecuteMsSqlCommand_NoMsg(str1);
+                MessageBox.Show("Sucessfully");
+            }
+        }
+
+        private void btnclear_Click(object sender, EventArgs e)
+        {
+            this.Form1_Load(sender, e);
+
+        }
+    }
+}
